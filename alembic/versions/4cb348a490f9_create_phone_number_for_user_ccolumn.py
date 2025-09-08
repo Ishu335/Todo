20b1e31ddb6fb49e7c9d0aa215a027f3e84adb33
Dexter◥▶_◀◤ <335ishwar@gmail.com>
2025-09-08 11:05:35 +0530

    Alembic Upgrade is Compleated Successfuled added a Phone NUmber column in User Tablee

---
+++ alembic/versions/4cb348a490f9_create_phone_number_for_user_ccolumn.py
@@ -0,0 +1,28 @@
+"""Create Phone number for user ccolumn
+
+Revision ID: 4cb348a490f9
+Revises: 
+Create Date: 2025-09-08 10:44:15.085957
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4cb348a490f9'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column('users',sa.Column('phone_number',sa.String(10),nullable=True))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from routers import auth,todos,admin,user
 
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
